0115ef72486df12419d8634db76fe89d268f9c3e
V0874 <visthemasteranon@gmail.com>
2026-01-06 23:03:30 -0600

    wrote two atomic write functions to keep our drivers clean and make writes a little faster

--- app_drivers/gpio/gpio.h
+++ app_drivers/gpio/gpio.h
@@ -1,4 +1,40 @@
+#ifndef GPIO_H
+#define GPIO_H
+
 #include "stm32f4xx_hal.h"
+
+
+/**
+ * @brief these are just two test functions
+ * 
+ */
 
 void led_flash();
 void gpio_init();
+
+
+/**
+ * @brief sets the pin high 
+ * 
+ * @param[in] port 
+ * @param[in] pin 
+ * @note atomic write
+ */
+
+static inline void set_pin_high(GPIO_TypeDef* port, uint16_t pin){
+    port->BSRR = (1 << pin);
+}
+
+/**
+ * @brief sets the pin low
+ * 
+ * @param[in] port 
+ * @param[in] pin
+ * @note atomic write 
+ */
+
+static inline void set_pin_low(GPIO_TypeDef* port, uint16_t pin){
+    port->BSRR = (1 << (pin+16));
+}
+
+#endif

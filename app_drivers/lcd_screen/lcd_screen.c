eb421c5dc8be6f40a7aaadaf7301a0b36832631e
V0874 <visthemasteranon@gmail.com>
2026-01-08 21:00:18 -0600

    wrapped up the final touches on the lcd driver. text is working and writes properly. read was not setup because it is simpler to just delay as we will only be writing to the display however it can be extended later to read the busy flag if necessary.

--- app_drivers/lcd_screen/lcd_screen.c
+++ app_drivers/lcd_screen/lcd_screen.c
@@ -1,17 +1,51 @@
 #include "lcd_screen.h"
 
 /**
- * @brief constants for commands to send to the display
+ * @brief control commands to configure the display
  * 
  */
 
+// clears the display and returns it to its original state
 static const uint8_t CLEAR_DISPLAY = 0x01;
+// returns the display to its original state if shifted
 static const uint8_t RETURN_HOME = 0x02;
-static const uint8_t ENTRY_MODE_SET = 0x04;
-static const uint8_t DISPLAY_CTRL = 0x08;
-static const uint8_t CURSOR_DISPLAY_SHIFT = 0x10;
-static const uint8_t FUNC_SET = 0x20;
-static const uint8_t SET_CGRAM_ADDR = 0x40;
+// configures the entry mode
+static const uint8_t ENTRY_MODE_SETUP = 0x04;
+// configures the display
+static const uint8_t DISPLAY_SETUP = 0x08;
+// configures cursor or display shift
+static const uint8_t CURSOR_DISPLAY_SHIFT_SETUP = 0x10;
+// configures data length / number of display lines / font
+static const uint8_t FUNC_SETUP = 0x20;
+// configures the cgram address and data is written/read from the mpu for cgram
+static const uint8_t CGRAM_ADDR_SETUP = 0x40;
+
+/**
+ * @brief secondary commands to use with the control commands
+ * 
+ */
+
+ // toggles display: 1 = on 0 = off
+ static const uint8_t DISPLAY_TOGGLE = 0x04;
+ // toggles cursor: 1 = on 0 = off
+ static const uint8_t CURSOR_TOGGLE = 0x02;
+ // toggles character indicated by cursor blink: 1 = on 0 = off
+ static const uint8_t BLINK_TOGGLE = 0x01;
+ /* toggles display shift to the right or left: 1 = shifts display to right 
+ if i/d is 0 / left if 0 ---- if 0, the display does not shift at all*/
+ static const uint8_t SHIFT_DISPLAY_TOGGLE = 0x01;
+ // toggles increment/decrement on character entry: 1 = increment 0 = decrement
+ static const uint8_t INCREMENT_DECREMENT_TOGGLE = 0x02; 
+ // toggles the display shift to the right or left: 1 = right 0 = left
+ static const uint8_t SHIFT_RL_TOGGLE = 0x04;
+ // toggles the display shift / cursor move: 1 = display shift 0 = cursor move
+ static const uint8_t SHIFT_CURSOR_TOGGLE = 0x08;
+ // toggles the number of display lines: 1 = 2 lines 0 = 1 line
+ static const uint8_t NUMBER_LINES_TOGGLE = 0x08;
+ // toggles the font pixel selection: 1 = 5 x 10 dots 0 = 5 x 8 dots
+ static const uint8_t FONT_TOGGLE = 0x04;
+ // toggles the data length: 1 = 8 bits 0 = 4 bits
+ static const uint8_t DATA_LENGTH_TOGGLE = 0x10;
 
 /**
  * @brief constants for masking data/commands into 4 bit values
@@ -24,10 +58,11 @@ static const uint8_t LOW_NIB_MASK = 0x0f;
 /**
  * @brief pulse to latch the data to the display
  * 
- * @param[in] cfg port/pin configuration 
+ * @param[in] cfg port/pin configuration
+ * @param[in] ms delay in miliseconds 
  */
 
-static void pulse(lcd_config_t *cfg, uint8_t ms){
+static void pulse(lcd_config_t *cfg, uint32_t ms){
     set_pin_high(cfg->e.port, cfg->e.pin);
     
     delay(ms);
@@ -38,7 +73,7 @@ static void pulse(lcd_config_t *cfg, uint8_t ms){
 }
 
 /**
- * @brief sends a 4 byte nibble to the databus
+ * @brief sends a 4 bit nibble to the databus
  * 
  * @param cfg port/pin configuration
  * @param[in] nib nibble to send 
@@ -55,34 +90,106 @@ static void send_nib(lcd_config_t *cfg, const uint8_t nib){
 }
 
 /**
- * @brief sends a command to send to the display
+ * @brief sends the high nibble only. this is needed for the initialization sequence.
  * 
- * @param[in] cfg port/pin configuration 
- * @param[in] command  command to send
+ * @param[in] cfg port/pin configuration
+ * @param[in] cmd command to send
  */
 
-static void send_command(lcd_config_t *cfg, const uint8_t command){
-    const uint8_t high_nib = (command & HIGH_NIB_MASK) >> 4;
-    const uint8_t low_nib = command & LOW_NIB_MASK;
+static void send_high_nib(lcd_config_t* cfg, const uint8_t cmd){
+    const uint8_t high_nib = (cmd & HIGH_NIB_MASK) >> 4;
 
     set_pin_low(cfg->rs.port, cfg->rs.pin);
+    set_pin_low(cfg->rw.port, cfg->rw.pin);
+
+    send_nib(cfg, high_nib);    
+}
+
+/**
+ * @brief sends a command for display configuration
+ * 
+ * @param[in] cfg port/pin configuration 
+ * @param[in] cmd  command to send
+ */
+
+static void send_command(lcd_config_t *cfg, const uint8_t cmd){
+    const uint8_t high_nib = (cmd & HIGH_NIB_MASK) >> 4;
+    const uint8_t low_nib = cmd & LOW_NIB_MASK;
+
+    set_pin_low(cfg->rs.port, cfg->rs.pin);
+    set_pin_low(cfg->rw.port, cfg->rw.pin);
 
     send_nib(cfg, high_nib);
     send_nib(cfg, low_nib);
 }
 
 /**
- * @brief sends a character to the display
+ * @brief runs the initialization sequence for the display
+ * 
+ * @param[in] cfg port/pin configuration 
+ */
+
+void lcd_init(lcd_config_t *cfg){
+    delay(15);
+    
+    // pulse 8 bit data length three times to set data length mode
+
+    send_high_nib(cfg, FUNC_SETUP | DATA_LENGTH_TOGGLE);
+
+    delay(5);
+
+    send_high_nib(cfg, FUNC_SETUP | DATA_LENGTH_TOGGLE);
+
+    delay(1);
+
+    send_high_nib(cfg, FUNC_SETUP | DATA_LENGTH_TOGGLE);
+
+    delay(1);
+
+    // sets 4 bit mode
+
+    send_high_nib(cfg, FUNC_SETUP);
+
+    delay(1);
+
+    send_command(cfg, FUNC_SETUP | (NUMBER_LINES_TOGGLE &~ FONT_TOGGLE));
+
+    delay(1);
+
+    // toggles the display off
+    send_command(cfg, DISPLAY_SETUP);
+    
+    delay(1);
+
+    // clears the display
+    send_command(cfg, CLEAR_DISPLAY);
+
+    delay(5);
+
+    // sets the entry mode, increment by 1, shift display off
+    send_command(cfg, ENTRY_MODE_SETUP | (INCREMENT_DECREMENT_TOGGLE &~ SHIFT_DISPLAY_TOGGLE));
+
+    delay(1);
+
+    // toggles display on
+    send_command(cfg, DISPLAY_SETUP | DISPLAY_TOGGLE);
+
+    delay(1);
+}
+
+/**
+ * @brief writes a character to the display
  * 
  * @param[in] cfg port/pin configuration
  * @param[in] ch char to send 
  */
 
-void send_char(lcd_config_t *cfg, uint8_t ch){
+void write_char(lcd_config_t *cfg, uint8_t ch){
     uint8_t high_nib = (ch & HIGH_NIB_MASK) >> 4;
     uint8_t low_nib = ch & LOW_NIB_MASK;
 
     set_pin_high(cfg->rs.port, cfg->rs.pin);
+    set_pin_low(cfg->rw.port, cfg->rw.pin);
 
     send_nib(cfg, high_nib);
     send_nib(cfg, low_nib);
@@ -109,52 +216,95 @@ void return_home(lcd_config_t *cfg){
 }
 
 /**
- * @brief select entry mode
+ * @brief setup entry mode configuration
  * 
- * @param[in] cfg port/pin configuration 
- */
+ * @param[in] cfg port/pin configuration
+ * @param[in] id increment/decrement cursor on read/write
+ * @param[in] s shifts display right/left on read/write
+ * @note increment = 1 decrement = 0
+ *       shift left = 1 shift right = 0
+ */      
 
-void entry_mode_set(lcd_config_t *cfg){
-    send_command(cfg, ENTRY_MODE_SET);
+void entry_mode_setup(lcd_config_t *cfg, uint8_t id, uint8_t s){
+    uint8_t entry_mode = ENTRY_MODE_SETUP;
+
+    if(id == 1) entry_mode |= INCREMENT_DECREMENT_TOGGLE;
+    if(s == 1) entry_mode |= SHIFT_DISPLAY_TOGGLE;
+    
+    send_command(cfg, entry_mode);
 }
 
 /**
- * @brief allows for display configuration
- * @note display on/off blinker on/off cursor on/off
+ * @brief setup display configuration
  * 
- * @param[in] cfg port/pin configuration 
+ * @param[in] cfg port/pin configuration
+ * @param[in] d toggles the display on/off
+ * @param[in] c toggles the cursor on/off 
+ * @param[in] b toggles blink cursor on/off
+ * @note 1 = display on 0 = display off
+ *       1 = cursor on 0 = display off
+ *       1 = blink on 0 = blink off
  */
 
-void display_ctrl_set(lcd_config_t *cfg){
-    send_command(cfg, DISPLAY_CTRL);
+void display_setup(lcd_config_t *cfg, uint8_t d, uint8_t c, uint8_t b){
+    uint8_t display_config = DISPLAY_SETUP;
+
+    if(d == 1) display_config |= DISPLAY_TOGGLE;
+    if(c == 1) display_config |= CURSOR_TOGGLE;
+    if(b == 1) display_config |= BLINK_TOGGLE;
+
+    send_command(cfg, display_config);
 }
 
 /**
- * @brief cursor/display shift configuration
+ * @brief setup cursor/display shift configuration
  * 
- * @param[in] cfg port/pin configuration 
+ * @param[in] cfg port/pin configuration
+ * @param[in] sc display shift / cursor move
+ * @param[in] rl shift to the right / left 
+ * @note sc: 0 rl: 0 = shifts the cursor to the left
+ *       sc: 0 rl: 1 = shifts the cursor to the right
+ *       sc: 1 rl: 0 shifts the entire display to the left - cursor follows
+ *       sc: 1 rl: 1 shifts the entire display to the right - cursor follows
  */
 
-void cursor_display_shift(lcd_config_t *cfg){
-    send_command(cfg, CURSOR_DISPLAY_SHIFT);
+void cursor_display_shift_setup(lcd_config_t *cfg, uint8_t sc, uint8_t rl){
+    uint8_t cursor_display_config = CURSOR_DISPLAY_SHIFT_SETUP;
+
+    if(sc == 1) cursor_display_config |= SHIFT_CURSOR_TOGGLE;
+    if(rl == 1) cursor_display_config |= SHIFT_RL_TOGGLE;
+
+    send_command(cfg, cursor_display_config);
 }
 
 /**
- * @brief allows for display function setup
+ * @brief setup function configuration
  * 
  * @param[in] cfg port/pin configuration 
+ * @param[in] dl data length
+ * @param[in] n number of lines
+ * @param[in] f font
+ * @note 8 bits = 1 / 4 bits = 0
+ *       2 lines = 1 / 1 line = 0
+ *       5 x 10 dots = 1 / 5 x 8 dots = 0
  */
 
-void func_set(lcd_config_t *cfg){
-    send_command(cfg, FUNC_SET);
+void func_setup(lcd_config_t *cfg, uint8_t dl, uint8_t n, uint8_t f){
+    uint8_t func_setup = FUNC_SETUP;
+
+    if(dl == 1) func_setup |= DATA_LENGTH_TOGGLE;
+    if(n == 1) func_setup |= NUMBER_LINES_TOGGLE;
+    if(f == 1) func_setup |= FONT_TOGGLE;
+
+    send_command(cfg, func_setup);
 }
 
 /**
- * @brief set the cgram address
+ * @brief setup cgram address
  * 
  * @param[in] cfg port/pin configuration 
  */
 
-void set_cgram_addr(lcd_config_t *cfg){
-    send_command(cfg, SET_CGRAM_ADDR);
+void cgram_addr_setup(lcd_config_t *cfg){
+    send_command(cfg, CGRAM_ADDR_SETUP);
 }
--- app_drivers/lcd_screen/lcd_screen.h
+++ app_drivers/lcd_screen/lcd_screen.h
@@ -19,7 +19,7 @@
 /**
  * @brief lcd reg/databus structure
  * @note 4 bit mode is supported for this implementation
- * @note 8 bit mode could be supported but would need a new command func
+ *       8 bit may be supported later but 4 bit is simpler and uses less pins
  */
 
 typedef struct{
@@ -28,6 +28,23 @@ lcd_t rw;
 lcd_t e;
 lcd_t db[4];
 } lcd_config_t;
+
+/**
+ * @brief runs the initialization sequence for the display
+ * 
+ * @param[in] cfg port/pin configuration 
+ */
+
+void lcd_init(lcd_config_t *cfg);
+
+/**
+ * @brief writes a character to the display
+ * 
+ * @param[in] cfg port/pin configuration
+ * @param[in] ch char to send 
+ */
+
+void write_char(lcd_config_t *cfg, uint8_t ch);
 
 /**
  * @brief clear the display
@@ -46,44 +63,65 @@ void clear_display(lcd_config_t *cfg);
 void return_home(lcd_config_t *cfg);
 
 /**
- * @brief select entry mode
+ * @brief setup entry mode configuration
  * 
- * @param[in] cfg port/pin configuration 
- */
+ * @param[in] cfg port/pin configuration
+ * @param[in] id increment/decrement cursor on read/write
+ * @param[in] s shifts display right/left on read/write
+ * @note increment = 1 decrement = 0
+ *       shift left = 1 shift right = 0
+ */      
 
-void entry_mode_set(lcd_config_t *cfg);
+void entry_mode_setup(lcd_config_t *cfg, uint8_t id, uint8_t s);
 
 /**
- * @brief allows for display configuration
- * @note display on/off blinker on/off cursor on/off
+ * @brief setup display configuration
  * 
- * @param[in] cfg port/pin configuration 
+ * @param[in] cfg port/pin configuration
+ * @param[in] d toggles the display on/off
+ * @param[in] c toggles the cursor on/off 
+ * @param[in] b toggles blink cursor on/off
+ * @note 1 = display on 0 = display off
+ *       1 = cursor on 0 = display off
+ *       1 = blink on 0 = blink off
  */
 
-void display_ctrl_set(lcd_config_t *cfg);
+void display_setup(lcd_config_t *cfg, uint8_t d, uint8_t c, uint8_t b);
 
 /**
- * @brief cursor/display shift configuration
+ * @brief setup cursor/display shift configuration
  * 
- * @param[in] cfg port/pin configuration 
+ * @param[in] cfg port/pin configuration
+ * @param[in] sc display shift / cursor move
+ * @param[in] rl shift to the right / left 
+ * @note sc: 0 rl: 0 = shifts the cursor to the left
+ *       sc: 0 rl: 1 = shifts the cursor to the right
+ *       sc: 1 rl: 0 shifts the entire display to the left - cursor follows
+ *       sc: 1 rl: 1 shifts the entire display to the right - cursor follows
  */
 
-void cursor_display_shift(lcd_config_t *cfg);
+void cursor_display_shift_setup(lcd_config_t *cfg, uint8_t sc, uint8_t rl);
 
 /**
- * @brief allows for display function setup
+ * @brief setup function configuration
  * 
  * @param[in] cfg port/pin configuration 
+ * @param[in] dl data length
+ * @param[in] n number of lines
+ * @param[in] f font
+ * @note 8 bits = 1 / 4 bits = 0
+ *       2 lines = 1 / 1 line = 0
+ *       5 x 10 dots = 1 / 5 x 8 dots = 0
  */
 
-void func_set(lcd_config_t *cfg);
+void func_setup(lcd_config_t *cfg, uint8_t dl, uint8_t n, uint8_t f);
 
 /**
- * @brief set the cgram address
+ * @brief setup cgram address
  * 
  * @param[in] cfg port/pin configuration 
  */
 
-void set_cgram_addr(lcd_config_t *cfg);
+void cgram_addr_setup(lcd_config_t *cfg);
 
 #endif

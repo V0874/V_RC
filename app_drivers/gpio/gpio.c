b731959020c1cccc012cf82504e7a5f3519f0212
V0874 <visthemasteranon@gmail.com>
2026-01-08 20:59:00 -0600

    updated

--- app_drivers/gpio/gpio.c
+++ app_drivers/gpio/gpio.c
@@ -22,9 +22,24 @@
     HAL_GPIO_Init(GPIOB, &lcd_gpio);
  }
 
+ /**
+  * @brief sets the pin high
+  * @note we don't want to clog our drivers up with HAL functions
+  * 
+  * @param[in] port  
+  * @param[in] pin 
+  */
+
 void set_pin_high(GPIO_TypeDef* port, uint16_t pin){
    HAL_GPIO_WritePin(port, pin, GPIO_PIN_SET);
 }
+
+/**
+ * @brief sets the pin low
+ * 
+ * @param[in] port 
+ * @param[in] pin 
+ */
 
 void set_pin_low(GPIO_TypeDef* port, uint16_t pin){
    HAL_GPIO_WritePin(port, pin, GPIO_PIN_RESET);
--- app_drivers/gpio/gpio.h
+++ app_drivers/gpio/gpio.h
@@ -4,13 +4,34 @@
 #include "stm32f4xx_hal.h"
 
 /**
- * @brief inits the pins needed for the display
+ * @brief initializes the pins needed for the lcd display
  * 
  */
 
  void lcd_gpio_init();
- void set_pin_high(GPIO_TypeDef* port, uint16_t pin);
- void set_pin_low(GPIO_TypeDef* port, uint16_t pin);
+
+ /**
+  * @brief sets the pin high
+  * @note we don't want to clog our drivers up with HAL functions
+  * 
+  * @param[in] port  
+  * @param[in] pin 
+  */
+
+void set_pin_high(GPIO_TypeDef* port, uint16_t pin){
+   HAL_GPIO_WritePin(port, pin, GPIO_PIN_SET);
+}
+
+/**
+ * @brief sets the pin low
+ * 
+ * @param[in] port 
+ * @param[in] pin 
+ */
+
+void set_pin_low(GPIO_TypeDef* port, uint16_t pin){
+   HAL_GPIO_WritePin(port, pin, GPIO_PIN_RESET);
+}
 
 
 #endif

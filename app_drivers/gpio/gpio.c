53c18a6bc3adc80bc8d8bacdd40d5a71c5560dba
V0874 <visthemasteranon@gmail.com>
2026-01-13 21:25:30 -0600

    updated pins. we added a SN74HCT245N and the voltage is nice, clean, and stable now. unfortunately we still have timing issues i believe. currently working on getting a microsecond timer running because we only have access to millisecond delay.

--- app_drivers/gpio/gpio.c
+++ app_drivers/gpio/gpio.c
@@ -1,26 +1,5 @@
 #include "gpio.h"
 
-/**
- * @brief inits the pins needed for the display
- * 
- */
-
- void lcd_gpio_init(){
-    GPIO_InitTypeDef lcd_gpio;
-
-    __HAL_RCC_GPIOB_CLK_ENABLE();
-
-    lcd_gpio.Pin = GPIO_PIN_6 | GPIO_PIN_7 |
-                   GPIO_PIN_8 | GPIO_PIN_9 |
-                   GPIO_PIN_12| GPIO_PIN_13;
-                   
-    lcd_gpio.Mode = GPIO_MODE_OUTPUT_OD;
-    lcd_gpio.Pull = GPIO_PULLUP;
-    lcd_gpio.Speed = GPIO_SPEED_FREQ_LOW;
-
-    HAL_GPIO_Init(GPIOB, &lcd_gpio);
- }
-
  /**
   * @brief sets the pin high
   * @note we don't want to clog our drivers up with HAL functions
@@ -43,3 +22,25 @@ void set_pin_high(GPIO_TypeDef* port, uint16_t pin){
 void set_pin_low(GPIO_TypeDef* port, uint16_t pin){
    HAL_GPIO_WritePin(port, pin, GPIO_PIN_RESET);
 }
+
+/**
+ * @brief inits the pins needed for lcd display
+ * 
+ */
+
+ void lcd_gpio_init(){
+    GPIO_InitTypeDef lcd_gpio;
+
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+
+    lcd_gpio.Pin = GPIO_PIN_6 | GPIO_PIN_7 |
+                   GPIO_PIN_8 | GPIO_PIN_9 |
+                   GPIO_PIN_12| GPIO_PIN_13|
+                   GPIO_PIN_14;
+                   
+    lcd_gpio.Mode = GPIO_MODE_OUTPUT_PP;
+    lcd_gpio.Pull = GPIO_NOPULL;
+    lcd_gpio.Speed = GPIO_SPEED_FREQ_LOW;
+
+    HAL_GPIO_Init(GPIOB, &lcd_gpio);
+ }

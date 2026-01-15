5019b796a19c2ae27aa3bc8b94c0e7070a1d885c
V0874 <visthemasteranon@gmail.com>
2026-01-15 10:37:05 -0600

    updated documentation for files and relocated some functions

--- app_drivers/timer/timer.c
+++ app_drivers/timer/timer.c
@@ -1,14 +1,20 @@
 #include "timer.h"
 
+/**
+ * @brief initializes lower level periphs - timer clock / gpio
+ * @note we don't need gpio as it is just a basic timer and we are just keeping time
+ * @param[in] timer timer configuration handle 
+ */
+
 void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *timer){
     __HAL_RCC_TIM4_CLK_ENABLE();
 }
 
-
- /**
-  * @brief initializes timer4
-  * 
-  */
+/**
+ * @brief initializes timer 4 - basic timer
+ * 
+ * @param[in] timer timer configuration handle 
+ */
 
   void timer4_init(TIM_HandleTypeDef *timer){
    timer->Instance = TIM4;
@@ -21,4 +27,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *timer){
    timer->Init.RepetitionCounter = 0x00;
 
    HAL_TIM_Base_Init(timer);
+   HAL_TIM_Base_Start(timer);
   }
+
+
--- app_drivers/timer/timer.h
+++ app_drivers/timer/timer.h
@@ -3,6 +3,11 @@
 
 #include "stm32f4xx_hal.h"
 
+/**
+ * @brief initializes timer 4 - basic timer
+ * 
+ * @param[in] timer timer configuration handle 
+ */
 
 void timer4_init(TIM_HandleTypeDef *timer);
 
